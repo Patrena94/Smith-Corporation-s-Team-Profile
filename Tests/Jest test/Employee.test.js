f69b85a28b34c1a1fbc8c1717a62a98b09508c59
Vashti <patrena@sbcglobal.net>
2021-08-01 11:16:21 -0700

    update code

--- Tests/Jest test/Employee.test.js
+++ Tests/Jest test/Employee.test.js
@@ -1,5 +1,13 @@
-const { test, expect } = require('@jest/globals');
-const checkIfEqual = require('../lib/index.js');
-test('checks if 10 is equal to 10', ()=> {
-    expect(checkIfEqual)(10, 10).toBe(true);
+// const { test, expect } = require('@jest/globals');
+// const checkIfEqual = require('.../lib/Employee');
+test ('creates a Employee object',()=> {
+    const Employee = new Employee('Carlton');
+    expect(Employee.name).toBe('Carlton');
+    expect(Employee.ID).toEqual(expect.any(Number));
+    expect(Employee.Email).toEqual(expect.any(Email));
+    expect(Employee.Edcuation).toEqual(expect.any(Education));
+    expect(Employee.title).toEqual(expect.any(title));
 });
+// test('checks if 10 is equal to 10', ()=> {
+//     expect(checkIfEqual)(10, 10).toBe(true);
+// });
